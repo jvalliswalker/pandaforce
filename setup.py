5a6303fdb10b01125864e85a3fa5ffda4720cd30
Jwok90 <jvalliswalker@gmail.com>
2019-04-20 12:45:45 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pandaforce",
+    version="0.0.1",
+    author="Jamil Vallis-Walker",
+    author_email="",
+    description="A pandas and simple-salesforce based utility package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jwok90/pandaforce",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

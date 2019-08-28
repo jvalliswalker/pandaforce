3466a57a6d29daa1a4f5d9a57a2aff874a64c58c
Jwok90 <jvalliswalker@gmail.com>
2019-08-28 08:59:49 -0700

    Update version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pandaforce-jwok90",
-    version="0.0.1",
+    version="0.0.2",
     author="Jamil Vallis-Walker",
     author_email="",
     description="A pandas and simple-salesforce based utility package",

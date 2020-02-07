338ed9ca5cf3834b19ef392d0b47f7473d3d692c
Jwok90 <jvalliswalker@gmail.com>
2020-02-07 09:21:39 -0800

    Version update
    Version update reflects change of `checkObject` function to `isObject`

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pandaforce",
-    version="1.0.0",
+    version="1.1.0",
     author="Jamil Vallis-Walker",
     author_email="",
     description="A pandas and simple-salesforce based utility package",
